deb8a433bf5eaa35f13b0fd70edded0bbe5b5198
Jayne Shin <shinjayne@gmail.com>
2018-11-12 16:44:43 +0900

    configure django admin site
    register all models in goods app to admin site
    
    register all models in user app to admin site

--- goods/admin.py
+++ goods/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from goods.models import Goods, Option, Shipping
+from goods.models import Goods, Option, Provider, Shipping
 
 
 # Register your models here.
 
-admin.register(Goods)
-admin.register(Option)
-admin.register(Shipping)
+admin.site.register(Goods)
+admin.site.register(Option)
+admin.site.register(Provider)
+admin.site.register(Shipping)
